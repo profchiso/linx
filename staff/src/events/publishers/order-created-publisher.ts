ccb7f9defd92caa55bd919d94d7f16c4ac0315b2
Okorie Chinedu <okoriechinedusunday@gmail.com>
2021-11-11 21:58:58 +0100

    remove irrelevant files

--- staff/src/events/publishers/order-created-publisher.ts
+++
@@ -1,5 +0,0 @@
-import { Publisher, OrderCreatedEvent, Subjects } from '@bc_tickets/common';
-
-export class OrderCreatedPublisher extends Publisher<OrderCreatedEvent> {
-  readonly subject = Subjects.OrderCreated;
-}
