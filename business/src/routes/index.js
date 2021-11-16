9ddea10a83bfd6c5087520b0d0e04eb4d4843605
Okorie Chinedu <okoriechinedusunday@gmail.com>
2021-11-16 21:21:27 +0100

    fix some issues

--- business/src/routes/index.js
+++ business/src/routes/index.js
@@ -73,60 +73,60 @@ businessRouter.post(
         }
 
         //upload images
-        console.log(req.files)
-        if (req.files.utilityBill) {
-            await cloudinary.uploader.upload(
-                req.files.utilityBill[0].path, {
-                    public_id: `utility-bill/${name.split(" ").join("-")}-utility-bill`,
-                },
-                (error, result) => {
-                    if (error)
-                        console.log("Error uploading utilityBill to cloudinary");
-                    imageData.utilityBill = result.secure_url;
+        // console.log(req.files)
+        // if (req.files.utilityBill) {
+        //     await cloudinary.uploader.upload(
+        //         req.files.utilityBill[0].path, {
+        //             public_id: `utility-bill/${name.split(" ").join("-")}-utility-bill`,
+        //         },
+        //         (error, result) => {
+        //             if (error)
+        //                 console.log("Error uploading utilityBill to cloudinary");
+        //             imageData.utilityBill = result.secure_url;
 
-                }
-            );
-        }
-        if (req.files.registrationCertificate) {
-            await cloudinary.uploader.upload(
-                req.files.registrationCertificate[0].path, {
-                    public_id: `registration-certificate/${name.split(" ").join("-")}-registration-certificate`,
-                },
-                (error, result) => {
-                    if (error)
-                        console.log("Error uploading registration Certificate to cloudinary");
-                    imageData.registrationCertificate = result.secure_url;
+        //         }
+        //     );
+        // }
+        // if (req.files.registrationCertificate) {
+        //     await cloudinary.uploader.upload(
+        //         req.files.registrationCertificate[0].path, {
+        //             public_id: `registration-certificate/${name.split(" ").join("-")}-registration-certificate`,
+        //         },
+        //         (error, result) => {
+        //             if (error)
+        //                 console.log("Error uploading registration Certificate to cloudinary");
+        //             imageData.registrationCertificate = result.secure_url;
 
-                }
-            );
-        }
-        if (req.files.otherDocuments) {
-            await cloudinary.uploader.upload(
-                req.files.otherDocuments[0].path, {
-                    public_id: `other-documents/${name.split(" ").join("-")}-other-documents`,
-                },
-                (error, result) => {
-                    if (error)
-                        console.log("Error uploading other Documents to cloudinary");
-                    imageData.otherDocuments = result.secure_url;
+        //         }
+        //     );
+        // }
+        // if (req.files.otherDocuments) {
+        //     await cloudinary.uploader.upload(
+        //         req.files.otherDocuments[0].path, {
+        //             public_id: `other-documents/${name.split(" ").join("-")}-other-documents`,
+        //         },
+        //         (error, result) => {
+        //             if (error)
+        //                 console.log("Error uploading other Documents to cloudinary");
+        //             imageData.otherDocuments = result.secure_url;
 
-                }
-            );
-        }
+        //         }
+        //     );
+        // }
 
-        if (req.files.tinCertificate) {
-            await cloudinary.uploader.upload(
-                req.files.tinCertificate[0].path, {
-                    public_id: `tin-certificate/${name.split(" ").join("-")}-tin-certificate`,
-                },
-                (error, result) => {
-                    if (error)
-                        console.log("Error uploading other Documents to cloudinary");
-                    imageData.tinCertificate = result.secure_url;
+        // if (req.files.tinCertificate) {
+        //     await cloudinary.uploader.upload(
+        //         req.files.tinCertificate[0].path, {
+        //             public_id: `tin-certificate/${name.split(" ").join("-")}-tin-certificate`,
+        //         },
+        //         (error, result) => {
+        //             if (error)
+        //                 console.log("Error uploading other Documents to cloudinary");
+        //             imageData.tinCertificate = result.secure_url;
 
-                }
-            );
-        }
+        //         }
+        //     );
+        // }
 
         //let userId = req.user.id
         //create business
