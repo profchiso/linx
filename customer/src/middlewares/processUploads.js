8da71680894664cfac4fa71e6fde0e13b7a4c0c0
Okorie Chinedu <okoriechinedusunday@gmail.com>
2021-12-16 16:17:43 +0100

    fix some issues

--- customer/src/middlewares/processUploads.js
+++ customer/src/middlewares/processUploads.js
@@ -1,21 +1,18 @@
-  exports.processCompanyLogo = async (req, res, next) => {
-    if (!req.file) return next();
-  
-    // company logo
-    req.body.companyLogo = req.file.path;
-  
-    next();
+  exports.processCompanyLogo = async(req, res, next) => {
+      if (!req.file) return next();
+
+      // company logo
+      req.body.companyLogo = req.file.path;
+
+      next();
   };
 
-  exports.processBase64CompanyLogo = async (req, res, next) => {
-    if (!req.body.companyLogo) return next();
-  
-    // company logo
-    req.body.companyLogo = res.secure_url;
-  
-    next();
+  exports.processBase64CompanyLogo = async(req, res, next) => {
+      if (!req.body.companyLogo) return next();
+
+      // company logo
+      console.log(res)
+      req.body.companyLogo = res.secure_url;
+
+      next();
   };
-
-
-  
-  
