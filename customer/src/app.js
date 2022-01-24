4a2e9698845adf6459d825c6c3229cfd39eb6687
Sunkanmi Olalude <olaludesunkanmi@yahoo.com>
2022-01-24 14:11:35 +0100

    fix bodyparser issue

--- customer/src/app.js
+++ customer/src/app.js
@@ -1,34 +1,34 @@
-const express = require("express")
+const express = require("express");
 
-const cors = require("cors")
+const cors = require("cors");
 
-require('express-async-errors');
+require("express-async-errors");
 
-const cookieSession = require('cookie-session');
-const { errorHandler, NotFoundError } = require('@bc_tickets/common');
-const db = require("../src/models/index")
+const cookieSession = require("cookie-session");
+const { errorHandler, NotFoundError } = require("@bc_tickets/common");
+const db = require("../src/models/index");
 
-const customerRouter = require('./routes/customer');
+const customerRouter = require("./routes/customer");
 
 const app = express();
-app.set('trust proxy', true);
-app.use(express.json());
-app.use(cors())
-app.use(express.urlencoded({ extended: true }))
+app.set("trust proxy", true);
+app.use(express.json({ limit: "50mb" }));
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
 app.use(
-    cookieSession({
-        signed: false,
-        secure: process.env.NODE_ENV != 'test',
-    })
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV != "test",
+  })
 );
 
 app.get("/", (req, res) => {
-    res.send("welcome to customer")
-})
+  res.send("welcome to customer");
+});
 app.use(customerRouter);
 
-app.all('*', async() => {
-    throw new NotFoundError();
+app.all("*", async () => {
+  throw new NotFoundError();
 });
 
 app.use(errorHandler);
