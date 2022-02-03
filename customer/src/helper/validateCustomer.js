3f810736f652850cb0ee782325e4dd63bab73b56
Sunkanmi Olalude <olaludesunkanmi@yahoo.com>
2022-02-03 16:05:35 +0100

    allow website field to be null

--- customer/src/helper/validateCustomer.js
+++ customer/src/helper/validateCustomer.js
@@ -1,47 +1,47 @@
-const Joi = require('@hapi/joi');
+const Joi = require("@hapi/joi");
 
 // validate customer data
 module.exports.validate = function validate(req) {
-    const schema = Joi.object({
-        businessName: Joi.string().required(),
-        businessEmail: Joi.string().required(),
-        businessPhoneNumber: Joi.string().required(),
-        website: Joi.string(),
-        companyLogo: Joi.string(),
-        address: Joi.string().required(),
-        country: Joi.string(),
-        state: Joi.string(),
-        lga: Joi.string(),
-        firstName: Joi.string().required(),
-        lastName: Joi.string(),
-        email: Joi.string().required(),
-        phoneNumber: Joi.string().required(),
-        alias: Joi.string().required(),
-        businessId: Joi.number().required()
-    });
+  const schema = Joi.object({
+    businessName: Joi.string().required(),
+    businessEmail: Joi.string().required(),
+    businessPhoneNumber: Joi.string().required(),
+    website: Joi.string().allow(""),
+    companyLogo: Joi.string().allow(""),
+    address: Joi.string().required(),
+    country: Joi.string(),
+    state: Joi.string(),
+    lga: Joi.string(),
+    firstName: Joi.string().required(),
+    lastName: Joi.string().allow(""),
+    email: Joi.string().required(),
+    phoneNumber: Joi.string().required(),
+    alias: Joi.string().required(),
+    businessId: Joi.number().required(),
+  });
 
-    return schema.validate(req);
+  return schema.validate(req);
 };
 
 //validate updated customer data
 module.exports.validateUpdate = function validate(req) {
-    const schema = Joi.object({
-        businessName: Joi.string(),
-        businessEmail: Joi.string(),
-        businessPhoneNumber: Joi.string(),
-        website: Joi.string(),
-        companyLogo: Joi.string(),
-        address: Joi.string(),
-        country: Joi.string(),
-        state: Joi.string(),
-        lga: Joi.string(),
-        firstName: Joi.string(),
-        lastName: Joi.string(),
-        email: Joi.string(),
-        phoneNumber: Joi.string(),
-        alias: Joi.string().required(),
-        businessId: Joi.number().required()
-    });
+  const schema = Joi.object({
+    businessName: Joi.string(),
+    businessEmail: Joi.string(),
+    businessPhoneNumber: Joi.string(),
+    website: Joi.string(),
+    companyLogo: Joi.string(),
+    address: Joi.string(),
+    country: Joi.string(),
+    state: Joi.string(),
+    lga: Joi.string(),
+    firstName: Joi.string(),
+    lastName: Joi.string(),
+    email: Joi.string(),
+    phoneNumber: Joi.string(),
+    alias: Joi.string().required(),
+    businessId: Joi.number().required(),
+  });
 
-    return schema.validate(req);
+  return schema.validate(req);
 };
