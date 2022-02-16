e960c2ace3fb75e536c712833d87412a3e9ce666
Okorie Chinedu <okoriechinedusunday@gmail.com>
2022-02-16 05:21:49 +0100

    fix some issues

--- business/src/routes/index.js
+++ business/src/routes/index.js
@@ -764,7 +764,7 @@ businessRouter.post(
             const createdUnregisteredBusiness = db.businesses.create({
                 ...req.body,
                 ...req.imageData,
-                userId: data.user.id || req.body.userId,
+                userId: req.body.userId, //data.user.id || req.body.userId,
                 businessCategory: "Unregistered",
                 businessLogo: imageData.businessLogo,
                 alias: alias.toUpperCase(),
@@ -774,7 +774,7 @@ businessRouter.post(
 
 
             //create business alias
-            const businesAlias = await db.aliases.create({ name: alias.toUpperCase(), businessId: createdUnregisteredBusiness.id, userId: data.user.id || userId })
+            const businesAlias = await db.aliases.create({ name: alias.toUpperCase(), businessId: createdUnregisteredBusiness.id, userId: req.body.userId, }) // data.user.id || userId
 
             //create business owners
             let partners = [];
@@ -1007,7 +1007,7 @@ businessRouter.post(
 
             let awsQueuePayload = {
                 businessId: createdUnregisteredBusiness.id,
-                userId: data.user.id || userId,
+                userId: req.body.userId, //data.user.id || userId,
                 alias: alias.toUpperCase(),
                 tradingName,
                 name: tradingName || "",
