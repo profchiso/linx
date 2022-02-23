27cec8d326ff314388a3113239c3adcabfb2b0c0
Okorie Chinedu <okoriechinedusunday@gmail.com>
2022-02-23 13:18:09 +0100

    fix some issues

--- staff/src/models/permissions.js
+++ staff/src/models/permissions.js
@@ -1,23 +1,27 @@
 'use strict';
 const {
-  Model
+    Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class permissions extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
+    class permissions extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+        }
     }
-  }
-  permissions.init({
-    role: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'permissions',
-  });
-  return permissions;
+    permissions.init({
+        role: DataTypes.STRING
+    }, {
+        sequelize,
+        modelName: 'permissions',
+    });
+    permissions.associate = function(models) {
+        permissions.belongsTo(models.roles, { foreignKey: 'id', as: 'roles' })
+
+    }
+    return permissions;
 };
--- staff/src/models/roles.js
+++ staff/src/models/roles.js
@@ -1,23 +1,29 @@
 'use strict';
 const {
-  Model
+    Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class roles extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
+    class roles extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+        }
     }
-  }
-  roles.init({
-    name: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'roles',
-  });
-  return roles;
+    roles.init({
+        name: DataTypes.STRING
+    }, {
+        sequelize,
+        modelName: 'roles',
+    });
+
+    roles.associate = function(models) {
+        roles.hasMany(models.staffs, { foreignKey: 'id', as: 'roles' })
+        roles.hasMany(models.permissions, { foreignKey: 'id', as: 'permissions' })
+    }
+
+    return roles;
 };
