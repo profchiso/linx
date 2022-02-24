69398d1dc40eb67d6dfba8c4aa00bd132fbc77c5
Okorie Chinedu <okoriechinedusunday@gmail.com>
2022-02-24 13:08:07 +0100

    fix some isssues

--- staff/src/routes/index.js
+++ staff/src/routes/index.js
@@ -253,15 +253,15 @@ staffRouter.get(
     async(req, res) => {
         //authenticate user
         try {
-            const { data } = await axios.get(`${AUTH_URL}`, {
-                    headers: {
-                        authorization: req.headers.authorization
-                    }
-                })
-                //check if user is not authenticated
-            if (!data.user) {
-                return res.status(401).send({ message: `Access denied, you are not authenticated`, statuscode: 401, errors: [{ message: `Access denied, you are not authenticated` }] });
-            }
+            // const { data } = await axios.get(`${AUTH_URL}`, {
+            //         headers: {
+            //             authorization: req.headers.authorization
+            //         }
+            //     })
+            //     //check if user is not authenticated
+            // if (!data.user) {
+            //     return res.status(401).send({ message: `Access denied, you are not authenticated`, statuscode: 401, errors: [{ message: `Access denied, you are not authenticated` }] });
+            // }
 
             const { businessId } = req.params;
             const staff = await db.staff.findAll({ where: { businessId } });
@@ -410,22 +410,67 @@ staffRouter.post(
     async(req, res) => {
 
         try {
-            //authenticate user
-            const { data } = await axios.get(`${AUTH_URL}`, {
-                    headers: {
-                        authorization: req.headers.authorization
-                    }
-                })
-                //check if user is not authenticated
-            if (!data.user) {
-                return res.status(401).send({ message: `Access denied, you are not authenticated`, statuscode: 401, errors: [{ message: `Access denied, you are not authenticated` }] });
+            const { staffId, password, businessAlias } = req.body
+
+
+
+
+
+            //CHECK IF USER EXIST
+            const existingUser = await db.User.findOne({ where: { staffId, businessAlias } });
+            if (!existingUser) {
+                return res.status(400).send({ message: `Invalid user credentials`, statuscode: 401, errors: [{ message: `Invalid user credentials` }] });
             }
 
-            const { staffId } = req.params;
+            //COMPARE ENTERED PASSWORD WITH HASHED PASSWORD
+            if (!(await decryptPassword(password, existingUser.password))) {
+                return res.status(400).send({ message: `Invalid user credentials`, statuscode: 401, errors: [{ message: `Invalid user credentials` }] });
 
-            const updatedStaff = await db.staff.update(req.body, { where: { id: staffId }, returning: true, plain: true })
+            }
 
-            res.status(200).send({ message: `Staff info updated`, statuscode: 200, data: { staff: updatedStaff[1] } });
+            //JWT PAYLOAD FOR SIGINED IN USER
+            const payLoad = {
+                user: {
+                    id: existingUser.id,
+                    role: existingUser.role,
+                    permissions: existingUser.permissions
+                },
+            };
+
+            let accessToken = await generateAccessToken(payLoad);
+            let verificationCode
+            let myBusinesses = []
+            if (existingUser.isVerified) {
+                verificationCode = existingUser.verificationCode
+
+                //get associated businesses
+                let { data } = await axios.get(`${BUSINESS_SERVICE_URL}/my-businesses/${existingUser.id}`, {
+                    headers: {
+                        authorization: `Bearer ${accessToken}`
+                    }
+                })
+                console.log("data", data)
+                myBusinesses = data.myBusinesses
+                console.log("my businesses", myBusinesses)
+            } else {
+                //REGENERATE VERIFICATION TOKEN ON SIGIN IF USER IS NOT VERIFIED
+                verificationCode = generateVerificationCode()
+                const mailOptions = {
+                    from: process.env.SENDER_EMAIL,
+                    to: existingUser.email,
+                    subject: `LinX Account`,
+                    text: `Dear, ${existingUser.firstName} your account with LinX has not been verified, Please use the code:${verificationCode} to verify your LinX account`,
+                };
+                const updatedUser = await db.User.update({ verificationCode }, { where: { id: existingUser.id }, returning: true, plain: true })
+
+                //SEND EMAIL CONTAINING VERIFICATION CODE TO USER
+                await sendMailWithSendgrid(mailOptions)
+            }
+            existingUser.password = undefined
+
+            existingUser.verificationCode = verificationCode
+
+            res.status(200).send({ message: "Signin successful", statuscode: 200, data: { user: existingUser, accessToken, verificationCode, myBusinesses } });
 
         } catch (error) {
             console.log(error)
