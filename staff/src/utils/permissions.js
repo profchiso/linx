fd6448116e4737b0d0c25a9aee71c3e3185f2e13
Okorie Chinedu <okoriechinedusunday@gmail.com>
2022-03-10 13:47:03 +0100

    modify permission

--- staff/src/utils/permissions.js
+++ staff/src/utils/permissions.js
@@ -1,20 +1,12 @@
-exports.permissions = [{
-        id: 1,
-        slug: "CREATE_STAFF",
-        name: "CREATE STAFF",
-        description: "User can create staff"
-    },
-    {
-        id: 2,
-        slug: "CREATE_WALLET",
-        name: "CREATE WALLET",
-        description: "User can create wallet"
-    },
-    {
-        id: 3,
-        slug: "UPDATE_BUSINESS",
-        name: "UPDATE BUSINESS",
-        description: "User can update business information"
-    },
+exports.permissions = [
+    { slug: "HANDLE_STAFF", name: "HANDLE STAFF", description: "User can create,edit,deactivate staff" },
+    { slug: "HANDLE_CUSTOMER", name: "HANDLE CUSTOMER", description: "User can create,edit,deactivate customer" },
+    { slug: "CREATE_BUSINESS_WALLET", name: "CREATE BUSINESS WALLET", description: "User can extra wallets for business" },
+    { slug: "TRANSFER_BUSINESS_FUNDS", name: "TRANSFER BUSINESS FUNDS", description: "User can transfer funds or make transactions" },
+    { slug: "HANDLE_BUSINESS_ACCOUNTING", name: "HANDLE BUSINESS ACCOUNTING", description: "Handle business accounting" },
+    { slug: "HANDLE_INVOICE", name: "HANDLE INVOICE", description: "User can create,edit,dectivate and view invoice" },
+    { slug: "HANDLE_PAYROLL", name: "HANDLE PAYROLL", description: "User can hadle payroll, pay other staff on business behalf with business funds" },
+    { slug: "HANDLE_BUSINESS_SERVICES", name: "HANDLE BUSINESS SERVICES", description: "Handle business services on befalf of business" },
+    { slug: "HANDLE_MARKET_PLACE", name: "HANDLE MARKET PLACE", description: "Handle markt place on behalf of business" },
 
 ]
