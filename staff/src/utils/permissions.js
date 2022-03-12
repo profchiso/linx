ba80e322af79a6f6ded472fc3b9c4c83ba5f5b09
Okorie Chinedu <okoriechinedusunday@gmail.com>
2022-03-12 21:42:50 +0100

    fix some issues

--- staff/src/utils/permissions.js
+++ staff/src/utils/permissions.js
@@ -1,12 +1,12 @@
 exports.permissions = [
-    { slug: "HANDLE_STAFF", name: "HANDLE STAFF", description: "User can create,edit,deactivate staff" },
-    { slug: "HANDLE_CUSTOMER", name: "HANDLE CUSTOMER", description: "User can create,edit,deactivate customer" },
-    { slug: "CREATE_BUSINESS_WALLET", name: "CREATE BUSINESS WALLET", description: "User can extra wallets for business" },
-    { slug: "TRANSFER_BUSINESS_FUNDS", name: "TRANSFER BUSINESS FUNDS", description: "User can transfer funds or make transactions" },
-    { slug: "HANDLE_BUSINESS_ACCOUNTING", name: "HANDLE BUSINESS ACCOUNTING", description: "Handle business accounting" },
-    { slug: "HANDLE_INVOICE", name: "HANDLE INVOICE", description: "User can create,edit,dectivate and view invoice" },
-    { slug: "HANDLE_PAYROLL", name: "HANDLE PAYROLL", description: "User can hadle payroll, pay other staff on business behalf with business funds" },
-    { slug: "HANDLE_BUSINESS_SERVICES", name: "HANDLE BUSINESS SERVICES", description: "Handle business services on befalf of business" },
-    { slug: "HANDLE_MARKET_PLACE", name: "HANDLE MARKET PLACE", description: "Handle markt place on behalf of business" },
+    { id: 1, slug: "HANDLE_STAFF", name: "HANDLE STAFF", description: "User can create,edit,deactivate staff" },
+    { id: 2, slug: "HANDLE_CUSTOMER", name: "HANDLE CUSTOMER", description: "User can create,edit,deactivate customer" },
+    { id: 3, slug: "CREATE_BUSINESS_WALLET", name: "CREATE BUSINESS WALLET", description: "User can extra wallets for business" },
+    { id: 4, slug: "TRANSFER_BUSINESS_FUNDS", name: "TRANSFER BUSINESS FUNDS", description: "User can transfer funds or make transactions" },
+    { id: 5, slug: "HANDLE_BUSINESS_ACCOUNTING", name: "HANDLE BUSINESS ACCOUNTING", description: "Handle business accounting" },
+    { id: 6, slug: "HANDLE_INVOICE", name: "HANDLE INVOICE", description: "User can create,edit,dectivate and view invoice" },
+    { id: 7, slug: "HANDLE_PAYROLL", name: "HANDLE PAYROLL", description: "User can hadle payroll, pay other staff on business behalf with business funds" },
+    { id: 8, slug: "HANDLE_BUSINESS_SERVICES", name: "HANDLE BUSINESS SERVICES", description: "Handle business services on befalf of business" },
+    { id: 9, slug: "HANDLE_MARKET_PLACE", name: "HANDLE MARKET PLACE", description: "Handle markt place on behalf of business" },
 
 ]
