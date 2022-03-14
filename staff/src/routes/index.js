f707675db32b3b019b85a53c3614e87cdf6f7ca2
Okorie Chinedu <okoriechinedusunday@gmail.com>
2022-03-14 14:46:55 +0100

    fix some issues

--- staff/src/routes/index.js
+++ staff/src/routes/index.js
@@ -129,6 +129,7 @@ staffRouter.post(
             } else {
                 return res.status(400).send({ message: `Invalid authSource in headers value, can only be staff or user`, statuscode: 400, errors: [{ message: `Invalid authSource query parameter value, can only be staff or user` }] });
             }
+            console.log(authUser.id)
 
 
 
