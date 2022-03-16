c9e14c6b4ada954d22d7ea487ce25bdf32d3752f
Sunkanmi Olalude <olaludesunkanmi@yahoo.com>
2022-03-16 12:16:19 +0100

    add authentication for user and staff

--- invoice/src/controllers/deactivateInvoice.js
+++ invoice/src/controllers/deactivateInvoice.js
@@ -1,35 +1,62 @@
 const { Invoice } = require("../models/invoice");
 const axios = require("axios");
 const { NotAuthorisedError } = require("@bc_tickets/common");
-const AUTH_URL = "https://linx-rds.herokuapp.com/api/v1/auth/authenticate";
+const AUTH_URL = process.env.AUTH_URL;
+const STAFF_AUTH_URL = process.env.STAFF_AUTH_URL;
 
 module.exports = async (req, res) => {
   try {
     //authenticate user
-    // const { data } = await axios.get(`${AUTH_URL}`, {
-    //         headers: {
-    //             authorization: req.headers.authorization
-    //         }
-    //     })
-    //     //check if user is not authenticated
-    // if (!data.user) {
-    //     throw new NotAuthorisedError()
-    // }
+    let authUser;
+    if (!req.headers.authsource) {
+      return res.status(400).send({
+        message: `authSource header required`,
+        statuscode: 400,
+        errors: [{ message: `authSource header required` }],
+      });
+    }
 
-    // //authenticate user
-    // const { data } = await axios.get(`${AUTH_URL}`, {
-    //   headers: {
-    //     authorization: req.headers.authorization,
-    //   },
-    // });
-    // //check if user is not authenticated
-    // if (!data.user) {
-    //   return res.status(401).send({
-    //     message: `Access denied, you are not authenticated`,
-    //     statuscode: 401,
-    //     errors: [{ message: `Access denied, you are not authenticated` }],
-    //   });
-    // }
+    if (req.headers.authsource.toLowerCase() === "user") {
+      const { data } = await axios.get(`${AUTH_URL}`, {
+        headers: {
+          authorization: req.headers.authorization,
+        },
+      });
+      //check if user is not authenticated
+      if (!data.user) {
+        return res.status(401).send({
+          message: `Access denied, you are not authenticated`,
+          statuscode: 401,
+          errors: [{ message: `Access denied, you are not authenticated` }],
+        });
+      }
+      authUser = data.user;
+    } else if (req.headers.authsource.toLowerCase() === "staff") {
+      const { data } = await axios.get(`${STAFF_AUTH_URL}`, {
+        headers: {
+          authorization: req.headers.authorization,
+        },
+      });
+      //check if user is not authenticated
+      if (!data.user) {
+        return res.status(401).send({
+          message: `Access denied, you are not authenticated`,
+          statuscode: 401,
+          errors: [{ message: `Access denied, you are not authenticated` }],
+        });
+      }
+      authUser = data.user;
+    } else {
+      return res.status(400).send({
+        message: `Invalid authSource in headers value, can only be staff or user`,
+        statuscode: 400,
+        errors: [
+          {
+            message: `Invalid authSource query parameter value, can only be staff or user`,
+          },
+        ],
+      });
+    }
 
     // invoice validation
 
